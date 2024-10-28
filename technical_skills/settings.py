e4b96dcb1b4a3d655bb0794ce50fa1565703170b
Elsie Nagawa <198elsie@gmail.com>
2024-10-28 14:36:52 +0000

    recreated my superuser account

--- technical_skills/settings.py
+++ technical_skills/settings.py
@@ -171,4 +171,10 @@ os.path.join(BASE_DIR, 'db.sqlite3'),
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
