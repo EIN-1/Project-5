4cf6d27915a946468098ca9a005f84e680d135f1
Elsie Nagawa <198elsie@gmail.com>
2024-10-30 10:20:51 +0000

    added products views and urls templates and created a products.html file.

--- products/views.py
+++ products/views.py
@@ -1,17 +1,16 @@
 #/workspace/Project-5/products/views.py
 from django.shortcuts import render
-from django.contrib import messages
-from django.db.models import Q
-from .models import *
+from .models import Product
 
 # Create your views here.
-""" A view to return the index page """
+""" A view to all products including sorting and search queries """
 def list_all_products(request):
     products = Product.objects.all()
-    total_products = products.count()
-context = {
-        'total_products': total_products,
-        'products': products,}
 
-return render(request, 'home/index.html', context)
+    context = {
+
+    'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
 
