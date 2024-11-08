953f094d2a1675c92f62a9cf82a46e5e4bf67bea
Elsie Nagawa <198elsie@gmail.com>
2024-11-08 23:34:53 +0000

    Adding cart items and cart

--- products/signals.py
+++ products/signals.py
@@ -3,11 +3,16 @@ from django.dispatch import receiver
 from .models import Cart, Product
 
 #Copy cart for an anonymous user to the database
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+from .models import Cart, CartItem, Product
+
 @receiver(user_logged_in)
 def move_cart_to_database(sender, user, request, **kwargs):
     session_cart = request.session.get('cart', [])
     if session_cart:
+        cart, created = Cart.objects.get_or_create(user=user)
         for product_id in session_cart:
             product = Product.objects.get(id=product_id)
-            cart, created = Cart.objects.get_or_create(user=user, product=product)
-        request.session['cart'] = []  # Clear the session cart
+            CartItem.objects.get_or_create(cart=cart, product=product)
+        request.session['cart'] = []  # Clear session cart after migration
