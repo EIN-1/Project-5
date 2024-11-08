f29fe3cef8f07b03a8fbcb0000036013432b4e0f
Elsie Nagawa <198elsie@gmail.com>
2024-11-08 22:24:36 +0000

    Create Shopping Cart Model Fixes #32

--- products/models.py
+++ products/models.py
@@ -1,5 +1,9 @@
 #/workspace/Project-5/products/models.py
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 # Create your models here.
@@ -25,3 +29,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.courseName
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items')
+    course = models.ForeignKey(Product, on_delete=models.PROTECT)
+    added_at= models.DateTimeField(auto_now_add=True)
