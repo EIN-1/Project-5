a3e14ae3faf8712a35ba6a98ad4ae7f45beb2c95
Elsie Nagawa <198elsie@gmail.com>
2024-11-08 20:04:59 +0000

    Fixing the login and signup page

--- technical_skills/settings.py
+++ technical_skills/settings.py
@@ -45,12 +45,7 @@ INSTALLED_APPS = [
 
     'allauth',
     'allauth.account',
-    'allauth.account',
     'bootstrap5',
-    'django_bootstrap5',
-    'crispy_forms',
-    'crispy_bootstrap5', 
-
     'crispy_forms',
     'crispy_bootstrap5',
 
