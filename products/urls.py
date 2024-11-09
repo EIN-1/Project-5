15ff4ad30a0c11cdb667453deefccd8a6b888d96
Elsie Nagawa <198elsie@gmail.com>
2024-11-09 05:11:13 +0000

    Added remove_from_cart functionality

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_detail, name='cart_detail'),
+    path('remove-from-cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('<int:id>/', views.retrieve_product, name='product-detail'),
     path('', views.list_all_products, name='products'),
 ]
