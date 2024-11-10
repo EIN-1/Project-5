8f481d2f4cbc4d732c970d0727d79a78bdc48cf5
Elsie Nagawa <198elsie@gmail.com>
2024-11-10 06:30:00 +0000

    Added order items to the order list

--- products/views.py
+++ products/views.py
@@ -180,13 +180,11 @@ def order_confirmation(request):
 @login_required
 def my_orders(request):
     orders = Order.objects.filter(user=request.user)
-    print(orders)
     return render(request, 'orders/orders.html', {'orders':orders})
 
 
 @login_required
-def order_details(request, id):
-    order = Order.objects.get(id=id)
+def order_details(request, order_id):
+    order = Order.objects.get(id=order_id)
     items = order.items.all()
-    print(items)
     return render(request, 'orders/order-detail.html', {'order': order, 'items':items})
