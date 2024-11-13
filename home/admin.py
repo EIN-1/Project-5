8df9784cb5f396e82c8d50e8e7fd2be98690e687
Elsie Nagawa <198elsie@gmail.com>
2024-11-13 14:25:01 +0000

    Added carousel to admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Carousel
 
 # Register your models here.
+@admin.register(Carousel)
+class CarouselAdmin(admin.ModelAdmin):
+    list_display = ('label', 'active',)
+    search_fields = ('label',)
+    list_filter = ('label','text',)
+    list_editable = ('active',)
+
+    # You can customize the display here if needed
+    ordering = ('label',)
--- home/models.py
+++ home/models.py
@@ -5,3 +5,4 @@ class Carousel(models.Model):
     imageUrl = models.URLField()
     label = models.CharField(max_length=255)
     text = models.CharField(max_length=255)
+    active = models.BooleanField(default=False)
